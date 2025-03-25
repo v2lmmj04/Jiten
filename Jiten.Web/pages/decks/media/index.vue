2fdd43c12c133d6726472cb9075b21c515e06631
Sirus <5435333+Sirush@users.noreply.github.com>
2025-03-25 01:13:14 +0100

    push files I forgot to push (oops)

---
+++ Jiten.Web/pages/decks/media/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import MediaList from '~/components/MediaList.vue';
+
+  useHead(() => {
+    return {
+      title: 'Media decks',
+    };
+  });
+</script>
+
+<template>
+  <MediaList />
+</template>
+
+<style scoped></style>
