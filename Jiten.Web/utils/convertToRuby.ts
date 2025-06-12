fa14cdf75de3119d6c533de8c245c68b301fa630
Sirus <5435333+Sirush@users.noreply.github.com>
2025-06-12 12:33:12 +0200

    Reduce furigana size

--- Jiten.Web/utils/convertToRuby.ts
+++ Jiten.Web/utils/convertToRuby.ts
@@ -3,7 +3,7 @@ export function convertToRuby(text: string): string {
 
   if (store.displayFurigana) {
     return text.replace(/([\u4E00-\u9FFF\uFF10-\uFF5A々]+)\[([\u3040-\u309F\u30A0-\u30FF]+)]/g, (_match, kanji, furigana) => {
-      return `<ruby lang="ja">${kanji}<rp>(</rp><rt>${furigana}</rt><rp>)</rp></ruby>`;
+      return `<ruby lang="ja">${kanji}<rp>(</rp><rt style="font-size: small">${furigana}</rt><rp>)</rp></ruby>`;
     });
   } else {
     return text.replace(/([\u4E00-\u9FFF\uFF10-\uFF5A]+)\[([\u3040-\u309F\u30A0-\u30FF]+)]/g, (_match, kanji, _furigana) => {
